0abb422f4ff362d257db9ae72b24c1d95e2dfe65
GabrielArcanghelo <arcanghelogabriel@gmail.com>
2025-11-19 16:47:03 -0300

    Add files via upload

---
+++ Estacionamento/script.js
@@ -0,0 +1,38 @@
+function fazerCadastro() {
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const confirmarSenha = document.getElementById("confirmarSenha").value;
+  const placa = document.getElementById("placa").value;
+
+  if (senha !== confirmarSenha) {
+    alert("As senhas não coincidem!");
+    return;
+  }
+
+  const usuario = { nome, email, senha, placa };
+
+  localStorage.setItem("usuario", JSON.stringify(usuario));
+
+  alert("Cadastro realizado com sucesso!");
+  window.location.href = "index.html";
+}
+
+function fazerLogin() {
+  const email = document.getElementById("emailLogin").value;
+  const senha = document.getElementById("senhaLogin").value;
+
+  const usuario = JSON.parse(localStorage.getItem("usuario"));
+
+  if (!usuario) {
+    alert("Nenhum usuário cadastrado!");
+    return;
+  }
+
+  if (email === usuario.email && senha === usuario.senha) {
+    alert("Login realizado!");
+    // redirecionar para dashboard
+  } else {
+    alert("Email ou senha incorretos!");
+  }
+}
